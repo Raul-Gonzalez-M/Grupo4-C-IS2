b9772a5b13e93c45ac6b7e77fe654ab5007891bd
Raúl <raulgonmu@gmail.com>
2023-03-22 13:35:40 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    /*private static Controller controller;
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.forLanguageTag("es-ES")); //alternativamente usar "-Duser.language=es -Duser.country=ES" como argumentos del programa
+
+        controller = new Controller(BackEnd.getInstance());
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainWindow(controller);
+            }
+        });
+    }*/
+}
